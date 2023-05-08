c01b0b4ff139e968f1e09491585f9cb245ba79e8
lc-masa <myamanakalife6911@gmail.com>
2023-05-08 10:17:15 +0900

    Updated with sales from April, 2023

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const colorBlue = '#36a2eb';
+const colorPink = '#ff6384';
+const colorGreen = '#36eb85';
+
+
 const ctx = document.getElementById('myChart');
 const data = {
   labels: ['4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月', '1月', '2月', '3月'],
@@ -5,14 +10,20 @@ const data = {
     {
       label: '目標',
       data: [1000000, 1000000, 900000, 1700000, 700000, 700000, 700000, 700000, 700000, 4200000, 1200000, 1200000],
-      borderColor: '#ff6384',
-      backgroundColor: '#ff6384',
+      borderColor: colorBlue,
+      backgroundColor: colorBlue,
+    },
+    {
+      label: '実績',
+      data: [1639000],
+      borderColor: colorPink,
+      backgroundColor: colorPink,
     },
     {
       label: '前年同月',
       data: [390000, 1071000, 800100, 1417500, 588252, 488503, 903000, 1019500, 2779100, 4900100, 2502200, 1443300],
-      borderColor: '#36a2eb',
-      backgroundColor: '#36a2eb',
+      borderColor: colorGreen,
+      backgroundColor: colorGreen,
     }
   ]
 };
@@ -45,14 +56,20 @@ const data2 = {
     {
       label: '目標',
       data: [100000, 100000, 200000, 200000, 250000, 300000, 300000, 300000, 300000, 300000, 300000, 500000],
-      borderColor: '#ff6384',
-      backgroundColor: '#ff6384',
+      borderColor: colorBlue,
+      backgroundColor: colorBlue,
+    },
+    {
+      label: '実績',
+      data: [107000],
+      borderColor: colorPink,
+      backgroundColor: colorPink,
     },
     {
       label: '前年同月',
       data: [149537, 140000, 152000, 704700, 30000, 81500, 292000, 165700, 30000, 98000, 65500, 713500],
-      borderColor: '#36a2eb',
-      backgroundColor: '#36a2eb',
+      borderColor: colorGreen,
+      backgroundColor: colorGreen,
     }
   ]
 };
